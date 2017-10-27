958aa71ff87fa8f60a2f4a7c728ba43adb078624
SANDEEP KP <sandeepkp07@gmail.com>
2017-10-27 15:09:24 +0530

    Update 14.1.py

--- oct21-22/14.1.py
+++ oct21-22/14.1.py
@@ -1,11 +1,5 @@
 import os
 def walk(updates):
- for root,f,name in os.walk(updates):
-  for fn in f:
-   print os.path.join(name,root,fn)
-  else:
-   return 1
-def walk1(updates):
  for name in os.listdir(updates):
   path = os.path.join(updates, name)
   if os.path.isfile(path):
@@ -13,5 +7,3 @@ def walk1(updates):
   else:
    walk(path)
 walk('/home/sandeep/updates')
-walk1('/home/sandeep/updates')
-
