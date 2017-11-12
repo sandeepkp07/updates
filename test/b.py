ebdfb53a4ee1a13bd0a04e2bcad26d1a7995227c
sandeepkp07 <sandeepkp07@gmail.com>
2017-11-12 22:47:09 +0530

    test

--- test/b.py
+++ test/b.py
@@ -1,21 +1,33 @@
 import sys
 class Complex:
- ""
-def add_mul(a,b):
- c=add(a,b)
- d=multiply(a,b)
- print (c.real,c.imag)
- return d.real,d.imag
-
-def add(self,other):
- c=complex(self.real+other.real,self.imag+other.imag)
- return c
-
-def multiply(self,other):
- d=complex(self.real*other.real,self.imag*other.imag)
- return d
+ def __init__(self,real,image):
+  self.real=real
+  self.image=image
+ 
+ def __str__(self):
+  c=a.add(b)
+  print '(%d,%d)' % (c.real,c.image) 
+  d=a.multiply(b)
+  return '(%d,%d)' % (d.real,d.image)
 
 
-a=complex(1,2)
-b=complex(3,4)
-print add_mul(a,b)
+ def add(self,other):
+  real=self.real+other.real
+  image = self.image+other.image
+  c=Complex(real,image)
+  return c
+
+
+
+ def multiply(self,other):
+  real=self.real*other.real
+  image=self.image*other.image
+  d=Complex(real,image)
+  return d
+
+
+
+
+a=Complex(1,2)
+b=Complex(3,4)
+print a
