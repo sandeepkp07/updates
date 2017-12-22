ed58e3de8327e973283f207512d3c09edd34183a
sandeepkp07 <sandeepkp07@gmail.com>
2017-12-22 13:06:19 +0530

    test

--- test_dec/ans3.py
+++ test_dec/ans3.py
@@ -1,33 +1,45 @@
 class Polynomial:
- def __init__(self,coefficient=0,exponent=0):
-  k=[]
-  z=[]
-  A=[]
-  b=[]
-  B=[]
+ def __init__(self,coe,coefficient=0,exponent=0):
+    k=[]
+    z=[]
+    A=[]
+    b=[]
+    B=[]
+    m=[]
+    f=coe.readlines()
+    for t in f:
+     k.append(t.strip().split('\n'))
+    for i in k:
+      r = filter(None,i)
+      for w in r:
+       l = str(filter(list,w))
+       z.append(l)
+    for v in z:
+     v.split()
+     (a,b) = v.split()
+     a = int(filter(str.isdigit,str(a)))
+     b=int(b)
+     A.append(a)
+     B.append(b)
+    coefficient= A
+    exponent = B
+    self.coefficient = coefficient
+    self.exponent = exponent
+    
+ def __str__(self):
   m=[]
-  f=open('q2.txt').readlines()
-  for t in f:
-   k.append(t.strip().split('\n'))
-  for i in k:
-    r = filter(None,i)
-    for w in r:
-     l = str(filter(list,w))
-     z.append(l)
-  for v in z:
-   v.split()
-   (a,b) = v.split()
-   a = str(filter(str.isdigit,str(a)))
-   A.append(a)
-   B.append(b)
-  coefficient = A
-  exponent = B
-  self.coefficient = coefficient
-  self.exponent = exponent
-  
+  d = "%sx^%s" % (self.coefficient,self.exponent)
+  m.append(d) 
+  print '+'.join(m) 
 
-def __str__(self):
- return self.coefficient
- 
-p1=Polynomial("q2.txt")
-print p1.coefficient()
+
+ def coefficient(self):
+  return self.coefficient
+
+ def exponent(self):
+  return self.exponent
+
+  
+p1=Polynomial(open("q2.txt"))
+print p1.coefficient()   
+print p1.exponent()                            
