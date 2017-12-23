741884eb8906b362f175b78711f0db88a691337a
SANDEEP KP <sandeepkp07@gmail.com>
2017-12-23 11:14:39 +0530

    Update ans2.py

--- test_dec/ans2.py
+++ test_dec/ans2.py
@@ -4,20 +4,20 @@ z=[]
 G={}
 f=open("q2.txt").readlines()
 for t in f:
- k.append(t.strip().split('\n'))
+   k.append(t.strip().split('\n'))
 for i in k:
- r = filter(None,i)
- for w in r:
-  l = str(filter(list,w))
-  z.append(l)
+   r = filter(None,i)
+   for w in r:
+      l = str(filter(list,w))
+      z.append(l)
 for v in z:
-  v.split()
-  (a,b) = v.split()
-  a = int(filter(str.isdigit,str(a)))
-  total=G.get(b,0) + a
-  G[b]=total
+   v.split()
+   (a,b) = v.split()
+   a = int(filter(str.isdigit,str(a)))
+   total=G.get(b,0) + a
+   G[b]=total
 for h in reversed(sorted(G.items())):
-  d = "%sx^%s" % (h[1],h[0])
-  m.append(d)
+   d = "%sx^%s" % (h[1],h[0])
+   m.append(d)
 print '+'.join(m)
 
